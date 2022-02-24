65eee774535cb6074fed3ffb60149c5988fcacf7
anggun123456 <332anggunseptiana@gmail.com>
2022-02-24 08:03:54 +0700

    commit

--- app/src/main/java/com/example/profilsekolahku/PerpusActivity.java
+++ app/src/main/java/com/example/profilsekolahku/PerpusActivity.java
@@ -2,13 +2,53 @@ package com.example.profilsekolahku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.profilsekolahku.Buku7Activity;
+import com.example.profilsekolahku.Buku8Activity;
+import com.example.profilsekolahku.Buku9Activity;
+import com.example.profilsekolahku.R;
+import com.example.profilsekolahku.SliderAdapter;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class PerpusActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.ekskul1,
+            R.drawable.ekskul2,
+            R.drawable.ekskul1,
+            R.drawable.ekskul2,
+            R.drawable.ekskul1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perpus);
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+
+    public void buku7(View view) {
+        Intent intent = new Intent(PerpusActivity.this, Buku7Activity.class);
+        startActivity(intent);
+    }
+
+    public void buku8(View view) {
+        Intent intent = new Intent(PerpusActivity.this, Buku8Activity.class);
+        startActivity(intent);
+    }
+
+    public void buku9(View view) {
+        Intent intent = new Intent(PerpusActivity.this, Buku9Activity.class);
+        startActivity(intent);
     }
 }
